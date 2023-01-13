56804fab5e3af50d86e0444780f3a6e872bf3028
dru0pa <dru0pa@gmail.com>
2023-01-13 14:25:57 +0200

    Update psmp-user-reset.sh

--- psmp-user-reset.sh
+++ psmp-user-reset.sh
@@ -4,7 +4,8 @@
 #The password for the accounts will still need to be changed making use of the PriverArk Client.
 #The server will pick up the hostname of the server placing that in the required place in the command to reset the password.
 #All accounts will have their password changed in the default location.
-#Make the required changes if the server is running RHEL8
+#Make the required changes if the server is running RHEL8 / CentOS /Rocky Linux
+#works with CyberArk 12.x
 #Script is written by Andrew 
 #email dru0pa@gmail.com
 
@@ -16,13 +17,24 @@ echo psmpgwuser.cred
 #Getting hostname and storing as the variable $HN
 HN=`hostname`
 echo Host name of the server is $HN
+#shorten account user name to 28 charachers
+echo PSMP_ADB_`hostname` | sed 's/\(.\{28\}\).*/\1/' > PSMP_ADB.txt
+
+echo PSMPApp_`hostname`| sed 's/\(.\{28\}\).*/\1/' > PSMPAPP.txt
+
+echo PSMPGW_`hostname` | sed 's/\(.\{28\}\).*/\1/' > PSMPGW.txt
+
+HN1=`cat PSMP_ADB.txt`
+HN2=`cat PSMPAPP.txt`
+HN3=`cat PSMPGW.txt`
+
 #requesting the password that will be used by the accounts as the variable $pass
 echo Type the password that will be used by all the accounts
 read pass
 #setting the password for the below accounts
-/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmpadb/vault/psmpadbridgeserveruser.cred Password -Username PSMP_ADB_$HN -Password $pass -EntropyFile
-/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmp/vault/psmpappuser.cred Password -Username PSMPAPP.$HN -Password $pass -EntropyFile
-/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmp/vault/psmpgwuser.cred  Password -Username PSMPGW_$HN -Password $pass -EntropyFile
+/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmpadb/vault/psmpadbridgeserveruser.cred Password -Username $HN1 -Password $pass -EntropyFile
+/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmp/vault/psmpappuser.cred Password -Username $HN2 -Password $pass -EntropyFile
+/opt/CARKpsmp/bin/createcredfile /etc/opt/CARKpsmp/vault/psmpgwuser.cred  Password -Username $HN3 -Password $pass -EntropyFile
 #Checking the files were updated with new time stamps
 echo ls of the files to see if there was a change
 ls -lsh /etc/opt/CARKpsmpadb/vault/psmpadbridgeserveruser.cred
@@ -43,5 +55,9 @@ service psmpsrv restart psmpadb
 #echo restarting both PSM for SSH and the PSM for SSH AD Bridge server
 #service psmpsrv restart
 #systemctl restart psmpsrv	#RHEL8
+#cleanup
+rm -rf PSMP_ADB.txt
+rm -rf PSMPAPP.txt
+rm -rf PSMPGW.txt
 
 echo Finished
